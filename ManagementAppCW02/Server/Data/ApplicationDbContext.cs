2bda144a2a6ce898773c1df9458ebc6232766ed1
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2021-12-14 12:44:08 +0530

    Add project files.

---
+++ ManagementAppCW02/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManagementAppCW02.Server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+    }
+}
---
+++ ManagementAppCW02/Shared/Models/CompanyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementAppCW02.Shared.Models
+{
+    public class CompanyModel
+    {
+        public Guid guid { get; set; }
+        public string companyName { get; set; }
+        public string companyType { get; set; }
+        public int numOfEmployees { get; set; } 
+    }
+}

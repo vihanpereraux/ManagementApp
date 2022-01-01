e924db0a932745921131717d9d9ef9d8f288ab45
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-01-01 17:03:55 +0530

    All chart generations added

---
+++ ManagmentAppTestOne/Client/wwwroot/complete_chart.js
@@ -0,0 +1,36 @@
+﻿function CompletedChart(total, state)
+{
+    am4core.useTheme(am4themes_animated);
+
+    var chart = am4core.create("chartdiv2", am4charts.PieChart);
+
+    // Add data
+    chart.data = [{
+        "category": "Other",
+        "litres": (total - state) / total * 100,
+        "color": "#e3e3e3"
+    }, {
+        "category": "Completed ",
+        "litres": state / total * 100,
+        "color": "#d9ccfc"
+    }
+    ];
+
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "litres";
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.propertyFields.fill = "color";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeOpacity = 1;
+    pieSeries.labels.template.disabled = false;
+
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+
+    chart.hiddenState.properties.radius = am4core.percent(0);
+
+    am4core.addLicense("ch-custom-attribution");
+}
